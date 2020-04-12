e05ddeda18c1e81dff85764f0626898cd0879cb5
Bob - Macbook <bdekonin@student.codam.nl>
2020-04-12 14:23:38 +0200

    42 header chagne, removed lib

--- utils.c
+++ utils.c
@@ -5,12 +5,11 @@
 /*                                                     +:+                    */
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2020/02/06 19:22:53 by bdekonin       #+#    #+#                */
-/*   Updated: 2020/04/03 13:02:58 by bdekonin      ########   odam.nl         */
+/*   Created: 2020/02/06 19:22:53 by bdekonin      #+#    #+#                 */
+/*   Updated: 2020/04/12 14:23:13 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "cub3d.h"
 
 int	ft_puterror(char *s)
