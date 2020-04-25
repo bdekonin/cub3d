965fe4a97c4792f11fe5a0b314585fb9376f0686
Bob - Macbook <bdekonin@student.codam.nl>
2020-04-25 15:48:58 +0200

    different error

--- parsing/gather_texture.c
+++ parsing/gather_texture.c
@@ -6,7 +6,7 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/03/12 12:34:25 by bdekonin      #+#    #+#                 */
-/*   Updated: 2020/04/15 21:41:52 by bdekonin      ########   odam.nl         */
+/*   Updated: 2020/04/21 22:24:30 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int				get_north(char *line, t_data *data)
 	data->north_path = read_filepath(line);
 	if (!data->north_path)
 	{
-		return (ft_puterror("Malloc has failed when creating the North path"));
+		return (ft_puterror("Something went wrong recieving the North path"));
 	}
 	else if (ft_strncmp(data->north_path, "file", 100) == 0)
 	{
@@ -40,7 +40,7 @@ int				get_east(char *line, t_data *data)
 	data->east_path = read_filepath(line);
 	if (!data->east_path)
 	{
-		return (ft_puterror("Malloc has failed when creating the East path."));
+		return (ft_puterror("Something went wrong recieving the East path."));
 	}
 	else if (ft_strncmp(data->east_path, "file", 100) == 0)
 	{
@@ -59,7 +59,7 @@ int				get_south(char *line, t_data *data)
 	data->south_path = read_filepath(line);
 	if (!data->south_path)
 	{
-		return (ft_puterror("Malloc has failed when creating the South path."));
+		return (ft_puterror("Something went wrong recieving the South path."));
 	}
 	else if (ft_strncmp(data->south_path, "file", 100) == 0)
 	{
@@ -78,7 +78,7 @@ int				get_west(char *line, t_data *data)
 	data->west_path = read_filepath(line);
 	if (!data->west_path)
 	{
-		return (ft_puterror("Malloc has failed when creating the West path."));
+		return (ft_puterror("Something went wrong recieving the West path."));
 	}
 	else if (ft_strncmp(data->west_path, "file", 100) == 0)
 	{
@@ -98,7 +98,7 @@ int				get_sprite(char *line, t_data *data)
 	if (!data->sprite_path)
 	{
 		return (
-	ft_puterror("Malloc has failed when creating the Sprite path."));
+	ft_puterror("Something went wrong recieving the Sprite path."));
 	}
 	else if (ft_strncmp(data->sprite_path, "file", 100) == 0)
 	{
